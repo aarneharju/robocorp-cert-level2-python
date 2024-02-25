bb07486b1ab65d773cbc5f7a857c7c48e55b4c58
aarneharju <35223549+aarneharju@users.noreply.github.com>
2024-02-25 21:02:33 +0200

    Orders csv file download working

--- tasks.py
+++ tasks.py
@@ -1,6 +1,39 @@
 from robocorp.tasks import task
+from robocorp import browser
+import requests
+
+
 
 @task
-def minimal_task():
-    message = "Hello"
-    message = message + " World!"
+def order_robots_from_RobotSpareBin():
+    """
+    Orders robots from RobotSpareBin Industries Inc.
+    Saves the order HTML receipt as a PDF file.
+    Saves the screenshot of the ordered robot.
+    Embeds the screenshot of the robot to the PDF receipt.
+    Creates ZIP archive of the receipts and the images. 
+    """
+    browser.configure(
+    browser_engine="chrome",
+    headless=False,
+    slowmo=1000
+    )
+
+    open_robot_order_website()
+    orders = get_orders()
+
+def open_robot_order_website():
+    """
+    Opens the robot order website.
+    """
+    browser.goto("https://robotsparebinindustries.com/")
+
+def get_orders():
+    """
+    Downloads the orders file
+    """
+    response = requests.get("https://robotsparebinindustries.com/orders.csv")
+    response.raise_for_status() # This will raise an exception if the request fails
+    with open("orders.csv", 'wb') as stream:
+        stream.write(response.content)
+    return "orders.csv"
